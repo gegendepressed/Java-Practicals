83701888013c265adb7032ca2536f57e3fb5b5ae
FrosT2k5 <androygaming@gmail.com>
2023-08-28 09:17:05 +0530

    compile.sh: Additional functionalities
    improving usage info
    allow using vim instead of nano
    also allow compiling directly without text editor
    
    Signed-off-by: FrosT2k5 <androygaming@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 setvim=0
-while getopts "v" flag; do
+while getopts "vc" flag; do
     case $flag in
         v)
-        setvim=1
-     esac
+        setvim=1 ;;
+        c)
+        setvim=2 ;;
+    esac
 done
 shift $(( OPTIND-1 ))
 
 
 if [ "$1" = "" ] ;then
-    echo "Usage: compile.sh folder/filename, compiles file and removes it after execution"
+    echo -e "Usage: $0 [args] <folder/filename>\n compiles file and removes it after execution \n\nArguments: \n -v: Use vim instead of nano \n -c: Compile without opening text editor"
     exit 1
 fi
 
 if [ $setvim -eq 1 ] ;then
     vim $1
+elif [ $setvim -eq 2 ] ;then
+    echo "Compiling $1"
 else
     nano $1
 fi
