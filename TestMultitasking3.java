eebc2caf00f310c1e78eb66337e028602563ef9f
Sairaj Pai <140620251+gegendepressed@users.noreply.github.com>
2023-10-24 09:07:53 +0530

    Add files via upload

---
+++ TestMultitasking3.java
@@ -0,0 +1,25 @@
+class Simple1 extends Thread{  
+ public void run(){ 
+   for (int i = 0; i<5; i++) { 
+     System.out.println("task one");  
+   }
+ }  
+}  
+  
+class Simple2 extends Thread{  
+ public void run(){  
+   for (int i = 0; i<5; i++) { 
+     System.out.println("task two");
+   }  
+ }  
+}  
+  
+ class TestMultitasking3 {  
+ public static void main(String args[]){  
+  Simple1 t1=new Simple1();  
+  Simple2 t2=new Simple2();  
+  
+  t1.start();  
+  t2.start();  
+ }  
+}  
